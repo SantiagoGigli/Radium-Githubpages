535a8f07f7430ddccbd603018726be3e3f09d1bb
SantiagoGigli <santigigli93@gmail.com>
2020-11-03 19:46:24 -0300

    alert added, bonus added, button on the main form added

--- MainWeb/JavaScript/app.js
+++ MainWeb/JavaScript/app.js
@@ -12,6 +12,11 @@ DNI: Número de 7 u 8 dígitos.
 */
 window.onload = function(){
 
+    //keydown function
+    document.getElementById('name').addEventListener('keyup',function(e){
+        document.getElementById('keydown').textContent = e.target.value;
+    })
+
     //Name Verification*************************
     document.getElementById('name').addEventListener('blur', validateName);
 
@@ -22,6 +27,7 @@ window.onload = function(){
         {
             document.getElementById('spanName').textContent = "Approved";
             document.getElementById('spanName').className = 'approvedText';
+            return nombre;
         }
         else{
             alert("The name must have at least 6 characters and only one space in between");
@@ -44,6 +50,7 @@ window.onload = function(){
         if (email.match(mailFormat)){
             document.getElementById('spanEmail').textContent = "Approved";
             document.getElementById('spanEmail').className = 'approvedText';
+            return email;
         }
         else{
             alert("Please insert a valid email format");
@@ -57,32 +64,49 @@ window.onload = function(){
         document.getElementById('spanEmail').className = 'removedText';
     }
 
-    //Password Verification*************************
-    var password;
-    document.getElementById('password').addEventListener('blur', validatePassword);
-    function validatePassword(){
-        password = document.getElementById('password').value;
+    // Password Validate
+    document.getElementById('password').addEventListener('blur',validatePassword);
+    function validatePassword() {
+        var password = document.getElementById('password').value;
         var validPassword = /[A-Za-z0-9]+/;
         var hasNumbers = /\d/;
-        if (password.length > 8 && password.match(validPassword) && password.match(hasNumbers)){
-            document.getElementById('spanPass').textContent = "Approved";
-            document.getElementById('spanPass').className = 'approvedText';
-            console.log(password);
+        if(password.length > 8 && password.match(validPassword) && password.match(hasNumbers)){
+            document.getElementById('spanPass').textContent = "Approved"
+            document.getElementById('spanPass').className = 'approvedText'
             return password;
-        }
-        else{
-            alert("The password must have at least 8 characters mixed with numbers");
-            document.getElementById('spanPass').textContent = "Not Approved";
-            document.getElementById('spanPass').className = 'errorText';
-        }
+        } else {
+            alert('Please insert a password valid format');
+            document.getElementById('spanPass').textContent = "Not Approved"
+            document.getElementById('spanPass').className = 'errorText'
+            }
     }
-    console.log(password);
+
     document.getElementById('password').addEventListener('focus', cleanPass);
     function cleanPass(){
         document.getElementById('spanPass').className = 'removedText';
     }
 
-    //Password Verification*************************
+    // Confirm Password
+    var password;
+    document.getElementById('repeat').addEventListener('blur', confirmPassword);
+    function confirmPassword(){
+        password = document.getElementById('password').value;
+        repeatPassword = document.getElementById('repeat').value;
+        if(document.getElementById('password').value == document.getElementById('repeat').value){
+            document.getElementById('spanRep').textContent = "Approved";
+            document.getElementById('spanRep').className = 'approvedText';
+            return password;
+        }
+        else {
+            alert('The password does not match');
+            document.getElementById('spanRep').textContent = "Not Approved";
+            document.getElementById('spanRep').className = 'errorText';
+        }
+    }
+    document.getElementById('repeat').addEventListener('focus', cleanRep);
+    function cleanRep(){
+        document.getElementById('spanRep').className = 'removedText';
+    }
 
     //Age Verification*************************
     document.getElementById('age').addEventListener('blur', validateAge);
@@ -97,6 +121,7 @@ window.onload = function(){
         else if(age.match(validAge)){
             document.getElementById('spanAge').textContent = "Approved";
             document.getElementById('spanAge').className = 'approvedText';
+            return age;
         }
     }
 
@@ -119,6 +144,7 @@ window.onload = function(){
         else if(phone.match(validPhone)){
             document.getElementById('spanPhone').textContent = "Approved";
             document.getElementById('spanPhone').className = 'approvedText';
+            return phone;
         }
     }
 
@@ -136,6 +162,7 @@ window.onload = function(){
         if (address.length > 5 && address.match(validAddress)){
             document.getElementById('spanAdd').textContent = "Approved";
             document.getElementById('spanAdd').className = 'approvedText';
+            return address;
         }
         else {
             alert("Insert a valid address format");
@@ -157,6 +184,7 @@ window.onload = function(){
         if (city.length > 3 && city.match(validCity)){
             document.getElementById('spanCity').textContent = "Approved";
             document.getElementById('spanCity').className = 'approvedText';
+            return city;
         }
         else {
             alert("The city must have at least 3 or more characters");
@@ -182,11 +210,50 @@ window.onload = function(){
         else {
             document.getElementById('spanCP').textContent = "Approved";
             document.getElementById('spanCP').className = 'approvedText';
+            return cp;
         }
     }
     document.getElementById('postalcode').addEventListener('focus', cleanCP);
     function cleanCP(){
         document.getElementById('spanCP').className = 'removedText';
+    }
+
+    //DNI Verification*************************
+    document.getElementById('dni').addEventListener('blur', validateDNI);
+    function validateDNI(){
+        var dni = document.getElementById('dni').value;
+        var validDNI = /^(0|[1-9]\d*)$/;
+        if (dni.length < 7 || isNaN(dni) || dni.length > 8 ){
+            alert("The DNI must have 7 or 8 numbers");
+            document.getElementById('spanDNI').textContent = "Not Approved";
+            document.getElementById('spanDNI').className = 'errorText';
+        }
+        else if(dni.match(validDNI)){
+            document.getElementById('spanDNI').textContent = "Approved";
+            document.getElementById('spanDNI').className = 'approvedText';
+            return dni;
+        }
+    }
+
+    document.getElementById('dni').addEventListener('focus', cleanDNI);
+    function cleanDNI(){
+        document.getElementById('spanDNI').className = 'removedText';
+    }
+
+    //Alert
+    document.getElementById('inputButton').addEventListener('click', finalResults);
+    function finalResults(){
+        var FullName = document.getElementById('name').value;
+        var Email = document.getElementById('email').value;
+        var Password = document.getElementById('password').value;
+        var RepeatPassword = document.getElementById('repeat').value;
+        var PhoneNumber = document.getElementById('phone').value;
+        var Address = document.getElementById('address').value;
+        var City = document.getElementById('city').value;
+        var PostalCode = document.getElementById('postalcode').value;
+        var DNI = document.getElementById('dni').value;
+        var Age = document.getElementById('age').value;
+        alert(`${FullName}\r\n${Email}\r\n${Password}\r\n${RepeatPassword}\r\n${PhoneNumber}\r\n${Address}\r\n${City}\r\n${PostalCode}\r\n${DNI}\r\n${Age}`);
     }
 }
 
